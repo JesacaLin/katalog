0cff75e23c7548bdcc5f02f8351e4a53096e0f16
JesacaLin <JesacaLin@gmail.com>
2023-01-15 20:01:39 -0500

    nav bar done in react.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar navbar-expand justify-content-between w-100">
+      <div className="container-fluid">
+        <h2 className="logoName ps-2">KATALOG</h2>
+        <div className="d-flex flex-row">
+          <button
+            className="button d-flex align-items-center fs-6 px-2"
+            id="login"
+          >
+            LOGIN
+          </button>
+          <button className="button d-flex align-items-center px-2">|</button>
+
+          <button
+            className="button d-flex align-items-center fs-6 px-2"
+            id="sign-up"
+          >
+            SIGN UP
+          </button>
+          <button className="button d-flex align-items-center px-2">
+            <img
+              src="/assets/toggle.svg"
+              className="add"
+              height="35"
+              alt="add"
+              id="toggle"
+            />
+          </button>
+          <a
+            href="https://github.com/JesacaLin/katalog"
+            target="_blank"
+            rel="noreferrer"
+            className="text-reset text-decoration-none d-flex align-items-center px-2 me-2"
+          >
+            <img
+              src="https://raw.githubusercontent.com/JesacaLin/dev-kitty/9b9c8e0d370ed7f93549104a1d86771704043b87/public/img/Github.svg"
+              className="add"
+              height="30"
+              alt="github link"
+            />
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -9,5 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
